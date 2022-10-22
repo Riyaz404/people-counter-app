84fb7c970310adcea584ab9fa7e8b653def219a3
urmil404 <urmil8989@gmail.com>
2022-10-22 18:41:37 +0530

    UI enhancement on App

--- app/src/main/java/riyaz/project/peoplecounter/MainActivity.kt
+++ app/src/main/java/riyaz/project/peoplecounter/MainActivity.kt
@@ -7,26 +7,24 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity()
-{
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var tvclicked=0
-        var tvIntput=findViewById<TextView>(R.id.count)
-        var btn:Button=findViewById<Button>(R.id.button) as Button
-        var btn2:Button=findViewById(R.id.reset) as Button
+        var tvclicked = 0
+        var tvIntput = findViewById<TextView>(R.id.count)
+        var btn: Button = findViewById<Button>(R.id.button) as Button
+        var txtReset: TextView = findViewById<TextView>(R.id.count) as TextView
 
 
         btn.setOnClickListener {
-            tvclicked+=1
-            tvIntput.text=tvclicked.toString()
-            }
-        btn2.setOnClickListener {
-            tvIntput.text="0"
-           tvclicked=0
-            Toast.makeText(applicationContext, "SHAIK RIYAZ", Toast.LENGTH_SHORT).show()
+            tvclicked += 1
+            tvIntput.text = tvclicked.toString()
+        }
+        txtReset.setOnClickListener {
+            tvclicked = 0
+            tvIntput.text = "0"
         }
     }
 
